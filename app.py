bd6c0f1b6d198de89c9ad19f91893a71e5879358
coppyfish-bit <coppy.fish@gmail.com>
2026-02-04 17:40:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Debug")
-st.title("🛠 デバッグ中")
-st.write("この文字が見えたら、認証後の描画は生きています！")
-st.success("ここまで動けば、あとはデータの読み込みだけです。")
+st.set_page_config(page_title="Reboot Test")
+st.title("🚀 復活テスト")
+st.balloons() # 画面に風船を飛ばします
+st.write("この画面が見えたら、ログイン機能は消去されています。")
