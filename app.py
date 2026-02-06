fcf9abb92a187ad8cd3d038640f55ae969a3a37c
coppyfish-bit <coppy.fish@gmail.com>
2026-02-06 16:30:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -234,7 +234,7 @@ st.markdown("""
        /* 120cmの場合、100 ÷ 12 = 8.333...% です */
     /* 線が数字より右にズレていくなら、この数字を「小さく」する（例: 8.31%） */
     /* 線が数字より左にズレていくなら、この数字を「大きく」する（例: 8.35%） */
-    background-size: 8.5% 100%, 4.166% 50% !important; 
+    background-size: 8.333% 100%, 4.166% 50% !important; 
 }
         background-position: left center !important;
         background-repeat: repeat-x !important;
@@ -352,6 +352,7 @@ if submit:
                 st.cache_data.clear()
             except Exception as e:
                 st.error(f"❌ 書き込みエラー: {e}")
+
 
 
 
