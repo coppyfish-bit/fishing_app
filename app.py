28ea7b363c1614d96ecaa760e4e3d3b43bed260c
coppyfish-bit <coppy.fish@gmail.com>
2026-02-12 11:43:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import time
 from streamlit_gsheets import GSheetsConnection
 from PIL import Image
 from PIL.ExifTags import TAGS, GPSTAGS
@@ -662,6 +663,7 @@ with tab3:
                 st.write("---")
         else:
             st.info("釣果データがありません。")
+
 
 
 
