6ff7716f722fcf496301fdccb0af9ebeb57d6557
coppyfish-bit <coppy.fish@gmail.com>
2026-02-12 11:37:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -72,13 +72,12 @@ def display_tide_graph(lat, lon, date_str, hit_time_str, tide_val, tide_phase):
         
 def upload_to_drive(uploaded_file):
     # Secretsから設定を読み込み（関数を呼ぶたびに確実に設定）
-    import cloudinary.uploader # 追加
     cloudinary.config(
-        cloud_name = st.secrets["cloudinary"]["dmkvcofvn"],
-        api_key = st.secrets["cloudinary"]["825168477246119"],
-        api_secret = st.secrets["cloudinary"]["w33OhZQVdivf4g2nq_LgxhkjnJk"],
-        secure = True
-    )
+    cloud_name = st.secrets["cloudinary"]["cloud_name"],
+    api_key = st.secrets["cloudinary"]["api_key"],
+    api_secret = st.secrets["cloudinary"]["api_secret"],
+    secure = True
+)
     
     # Cloudinaryへアップロード
     response = cloudinary.uploader.upload(
@@ -663,6 +662,7 @@ with tab3:
                 st.write("---")
         else:
             st.info("釣果データがありません。")
+
 
 
 
